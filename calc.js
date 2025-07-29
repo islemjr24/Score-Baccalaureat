3cc90d47528e4a01d7210baa7d678b8d1276165b
Brini_Islem <96793545+islemjr24@users.noreply.github.com>
2025-07-29 17:41:59 +0100

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,3 +1,27 @@
-function dem(){
-    alert("en cours de dévolepoment !!")
+function dem(ch){
+    info=[3,3,3,2,1,1,1,1,1,1];
+    math=[4,4,1,1,1,1,1,1,1,1];
+    if(ch=="info"){
+        i=0
+        s=0
+        c=0
+        while (i < info.length){
+            if(i==info.length-1){
+                if(Number(document.getElementById("z"+String(i)).value)>10){
+                    s=s+(Number(document.getElementById("z"+String(i)).value)*info[i])
+                    c=c+info[i]
+                    i++
+                }
+            }
+            else{
+                s=s+(Number(document.getElementById("z"+String(i)).value)*info[i])
+                c=c+info[i]
+                i++
+            }
+        }
+        moyenne=s/c
+        document.getElementById("moyenne").value=moyenne.toFixed(2)
+
+
+    }
 }
